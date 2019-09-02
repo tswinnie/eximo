d0c9f5bae8d30e771671882599ede9f08fc43a92
Tyrone Swinnie <tyrone.swinnie@gmail.com>
2019-09-02 19:37:52 -0400

    Revert "Feature splashscreen"

--- eximo/eximo/eximo.Android/MainActivity.cs
+++ eximo/eximo/eximo.Android/MainActivity.cs
@@ -9,18 +9,18 @@ using Android.OS;
 
 namespace eximo.Droid
 {
-    [Activity(Label = "eximo", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "eximo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-            base.SetTheme(Resource.Style.MainTheme);
+
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);       
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState); 
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
